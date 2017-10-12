2761f131b8d623a8e0f57bbeaa6c8d3e8f3c7df1
kamalthakur <kamalthakur2195@gmail.com>
2017-10-12 12:48:52 +0530

    added test cases

--- custom_label_image.py
+++ custom_label_image.py
@@ -1,3 +1,43 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Simple image classification with Inception.
+
+Run image classification with your model.
+
+This script is usually used with retrain.py found in this same
+directory.
+
+This program creates a graph from a saved GraphDef protocol buffer,
+and runs inference on an input JPEG image. You are required
+to pass in the graph file and the txt file.
+
+It outputs human readable strings of the top 5 predictions along with
+their probabilities.
+
+Change the --image_file argument to any jpg image to compute a
+classification of that image.
+
+Example usage:
+python label_image.py --graph=retrained_graph.pb
+  --labels=retrained_labels.txt
+  --image=flower_photos/daisy/54377391_15648e8d18.jpg
+
+NOTE: To learn to use this file and retrain.py, please see:
+
+https://codelabs.developers.google.com/codelabs/tensorflow-for-poets
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -38,14 +78,17 @@ parser.add_argument(
 
 
 def load_image(filename):
+  """Read in the image_data to be classified."""
   return tf.gfile.FastGFile(filename, 'rb').read()
 
 
 def load_labels(filename):
+  """Read in labels, one label per line."""
   return [line.rstrip() for line in tf.gfile.GFile(filename)]
 
 
 def load_graph(filename):
+  """Unpersists graph from file as default graph."""
   with tf.gfile.FastGFile(filename, 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
@@ -55,11 +98,14 @@ def load_graph(filename):
 def run_graph(image_data, labels, input_layer_name, output_layer_name,
               num_top_predictions):
   with tf.Session() as sess:
-
+    # Feed the image_data as input to the graph.
+    #   predictions will contain a two-dimensional array, where one
+    #   dimension represents the input image count, and the other has
+    #   predictions per class
     softmax_tensor = sess.graph.get_tensor_by_name(output_layer_name)
     predictions, = sess.run(softmax_tensor, {input_layer_name: image_data})
 
-
+    # Sort to show labels in order of confidence
     top_k = predictions.argsort()[-num_top_predictions:][::-1]
     for node_id in top_k:
       human_string = labels[node_id]
@@ -70,6 +116,7 @@ def run_graph(image_data, labels, input_layer_name, output_layer_name,
 
 
 def main(argv):
+  """Runs inference on an image."""
   if argv[1:]:
     raise ValueError('Unused Command Line Args: %s' % argv[1:])
 
@@ -82,28 +129,19 @@ def main(argv):
   if not tf.gfile.Exists(FLAGS.graph):
     tf.logging.fatal('graph file does not exist %s', FLAGS.graph)
 
-
+  # load image
   image_data = load_image(FLAGS.image)
 
+  # load labels
   labels = load_labels(FLAGS.labels)
 
+  # load graph, which is stored in the default session
   load_graph(FLAGS.graph)
 
   run_graph(image_data, labels, FLAGS.input_layer, FLAGS.output_layer,
             FLAGS.num_top_predictions)
-  print('\n\n\nC0 -> Normal Behaviour')
-  print('C1 -> Right Hand Texting')
-  print('C2 -> Right Side Talking on Phone')
-  print('C3 -> Left Hand Texting')
-  print('C4 -> Left Side Talking on Phone')
-  print('C5 -> Right hand over Music Player')
-  print('C6 -> Drinking with Right Hand')
-  print('C7 -> Picking from behind with Right Hand')
-  print('C8 -> One hand on Stearing')
-  print('C9 -> Talking to another Person')
 
 
 if __name__ == '__main__':
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=sys.argv[:1]+unparsed)
-
